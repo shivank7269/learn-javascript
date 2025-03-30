7ed066cb1d497bd38a8555e5f88e6194cbe83021
shivank7269 <singhshivank72@gmail.com>
2025-03-30 23:18:34 +0530

    Learn JS Dates

---
+++ 01basics/08_date.js
@@ -0,0 +1,13 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/100));
+
+console.log(myDate.getFullYear());
