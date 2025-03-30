22c1944dab1c8a85f1a6fba754ae7e14afd9fc75
shivank7269 <78946847+shivank7269@users.noreply.github.com>
2025-03-30 15:00:06 +0000

    Datatype in JavaScript

---
+++ 01basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all the JS code as newer version
+
+console.log(3+2) 
+console.log("Shivank");
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof ("shivank"))
+console.log(typeof "shivak");
+console.log(typeof 141646466);
+console.log(typeof true);
+
+
+
+
+
+
+
