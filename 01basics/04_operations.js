d69e30ce166546d3f731be9e03bcc47c54a5cf70
shivank7269 <singhshivank72@gmail.com>
2025-03-30 22:04:03 +0530

    Learn Operations in JavaScript

---
+++ 01basics/04_operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negvalue = -value
+
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 = " shivank"
+
+// console.log(str1+str2);
+// console.log(1+"2");
+// console.log("1"+3+4+5+6);    //if starting is string whole work as string
+
+// console.log(1+3+3+"4");  // if starting is int it add till integer and then concatenate
+console.log(+false) //+true gives 1 and +false gives 0
+console.log(+"");
+
+// let string = "shivank"
+// console.log(+"");
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.table([num1,num2,num3]);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
