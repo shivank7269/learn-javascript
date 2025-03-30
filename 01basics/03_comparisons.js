a16f990aade7888097e827a01f55d391b106abec
shivank7269 <singhshivank72@gmail.com>
2025-03-30 21:48:48 +0530

    Learn Conversion in JavaScript

---
+++ 01basics/03_comparisons.js
@@ -0,0 +1,13 @@
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=>true/0=>false
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
