f5fa546e544e905ac3e4071a71ac5ead349bac4e
shivank7269 <78946847+shivank7269@users.noreply.github.com>
2025-03-30 14:43:31 +0000

    Learn Basic variable of JavaScript

---
+++ 01basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "singhshivank72@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId=2  we cannot change the const value
+console.log(accountId);
+console.log(accountEmail);
+
+accountEmail="anc@hmail.com"
+accountPassword="21231313"
+accountCity="Benguluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
