6a8532d9f311de92c4071fae6fc03f31694f6ea2
shivank7269 <singhshivank72@gmail.com>
2025-03-31 21:02:10 +0530

    Learn about functions in JS

---
+++ 03_basics/01_function.js
@@ -0,0 +1,59 @@
+function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("V");
+    console.log("A");
+    console.log("N");
+    console.log("K");
+}
+// sayMyName()
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2)
+// }
+// function addTwoNumbers(number1,number2){
+//     return (number1+number2)
+// }
+
+// let result =addTwoNumbers(3,5);
+// console.log(result);
+
+
+function loginUserMeassage(username){
+    if(!username){
+        console.log("Please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+let result = loginUserMeassage("shivank")
+// console.log(result);
+
+function calculateCartPrice(...num1){ // ...this is called both rest and spread
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,600,400,800))
+
+const user = {
+    username:"shivank",
+    price:199
+}
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user);
+
+
+function a(params) {
+    let a=10
+    const b=20
+    var c=30
+
+}
+a();
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
