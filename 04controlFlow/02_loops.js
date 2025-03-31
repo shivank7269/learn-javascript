ceda3e2fedfb3a3a3712f629d037c52c4a7fa45b
shivank7269 <singhshivank72@gmail.com>
2025-03-31 23:03:41 +0530

    Learn Loops in JS

---
+++ 04controlFlow/02_loops.js
@@ -0,0 +1,17 @@
+//for loop
+for(let i=0;i<10;i++){
+    // console.log(i);
+}
+//while loop
+let t=10
+console.log(t);
+while(t-->0){
+    // console.log(10-t); 
+}
+
+//do while
+let score = 11
+do{
+    console.log(score);
+    score--
+}while(score<10)
